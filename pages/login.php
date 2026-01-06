59ddf2618c5909dbdcb66b655c7f2fa3d064bf81
Your Name <eriegian457@gmail.com>
2026-01-06 08:34:36 +0800

    new changes

--- pages/login.php
+++ pages/login.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHAFF GPT</title>
     <link rel="stylesheet" href="../css/loginstyle.css">
 </head>
+
 <body>
     <h1>Login</h1>
     <form action="">
-        <label for="">Username</label>
-        <input type="text" id="username" class="username" name="username">
+        <label for="" class="Username">Username</label>
+        <input type="text" id="username" class="Username" name="username">
         <label for="">Password</label>
         <input type="Password" id="Password" class="Password" name="Password">
         <button type="submit">Submit</button>
     </form>
 </body>
+
 </html>
